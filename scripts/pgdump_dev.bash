d27ada3784e708ca64ebb706c57c3d9ec227c20d
s1monj <simon@whitebrick.com>
2025-10-27 12:21:23 -0400

    fix: moved .env

--- scripts/pgdump_dev.bash
+++ scripts/pgdump_dev.bash
@@ -3,9 +3,9 @@ if [[ $(basename $(pwd)) == "scripts" ]]; then
   echo -e "\nRun this script from the parent directory\n"
   exit 1
 fi
-if [ ! -f ".env" ]; then
-  echo -e "\nCan not find .env\n"
+if [ ! -f "./jhe/.env" ]; then
+  echo -e "\nCan not find ./jhe/.env\n"
   exit 2
 fi
-export $(cat .env | sed 's/ /-/g' | sed 's/#.*//g' | xargs)
+export $(cat ./jhe/.env | sed 's/ /-/g' | sed 's/#.*//g' | xargs)
 PGPASSWORD=$DB_PASSWORD pg_dump -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -F plain --column-inserts --data-only > dump.txt
