cab86e6873d1a47162e3be1b86cb666f54506df2
Min RK <benjaminrk@gmail.com>
2026-02-10 13:35:32 -0800

    basic practitioner tests
    create doesn't work

---
+++ tests/test_practitioner_viewset.py
@@ -0,0 +1,60 @@
+import pytest
+from rest_framework.test import APIClient
+
+from .utils import fetch_paginated
+
+
+def test_list_practitioners(api_client, user):
+    practitioners = fetch_paginated(api_client, "/api/v1/practitioners")
+    assert len(practitioners) == 1
+    assert practitioners[0]["id"] == user.practitioner.id
+    # todo: pagination
+
+
+def test_list_practitioners_no_auth(user):
+    api_client = APIClient()
+    r = api_client.get("/api/v1/practitioners")
+    assert r.status_code == 401
+
+
+# fails with:
+# django.db.utils.IntegrityError: null value in column "birth_date" of relation "core_patient" violates not-null constraint
+# but shouldn't create a patient at all
+@pytest.mark.xfail("create practitioner doesn't work")
+def test_create_delete(api_client, organization):
+    email = "testcreate-practitioner@example.com"
+    r = api_client.post(
+        "/api/v1/practitioners",
+        {
+            "organizationId": organization.id,
+            "telecomEmail": email,
+            "nameFamily": "last",
+            "nameGiven": "first",
+            # "birthDate": "2000-01-01",
+        },
+        content_type="application/json",
+    )
+    assert r.status_code == 200, r.text
+    info = r.json()
+    assert "id" in info
+    assert info["telecomEmail"] == email
+    assert info["organizations"]
+    assert info["organizations"][0]["id"] == organization.id
+    r = api_client.get(f"/api/v1/practitioners/{info['id']}")
+    assert r.status_code == 200, r.text
+    assert r.json() == info
+
+    r = api_client.delete(f"/api/v1/practitioners/{info['id']}?organization_id={organization.id}")
+    assert r.status_code == 200, r.text
+    assert r.json()["success"]
+
+
+def test_create_invalid(api_client, organization):
+    r = api_client.post(
+        "/api/v1/practitioners",
+        {
+            "organizationId": organization.id,
+        },
+        content_type="application/json",
+    )
+    assert r.status_code == 400
